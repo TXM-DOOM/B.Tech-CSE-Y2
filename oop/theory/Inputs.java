8d0562ce94d311c384f6c7200cc491dc24537caa
Desh Iyer <desh.iyer@gmail.com>
2021-08-12 10:52:39 +0530

    Update oop

--- oop/theory/Inputs.java
+++
@@ -1,7 +0,0 @@
-// Demonstrating how to take an input from the standard input device
-
-public class Inputs {
-    public static void main(String[] args) {
-        System.out.printf("Hello, %s", args[0]);
-    }
-}
