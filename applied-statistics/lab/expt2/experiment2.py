536387cda2bdf92cc7da33c1211e15c9aed47775
Desh Iyer <desh.iyer@gmail.com>
2021-08-26 19:43:56 +0530

    Update

---
+++ applied-statistics/lab/expt2/experiment2.py
@@ -0,0 +1,41 @@
+# Code by Desh Iyer
+
+
+# ----------------------------------------------------------------------------
+
+
+# PROBLEM STATEMENT
+# For the data in Lab 1, find the range, standard deviation and variance for each of the variables. (Discuss measures of dispersion for Nominal and Ordinal Data)
+
+
+# ----------------------------------------------------------------------------
+
+
+# EXPLANATION
+# The data set has the following fields: Name, Marks, CGPI & Quality of Assignment
+
+# Name - this field is of nominal type. Hence no operations can be performed on it and no measures of spread can be obtained from it
+
+# Marks - this field is of ordinal type. Hence only operations of comparison can be performed. The range of this data can be found out but variance and std. deviation cannot be found.
+
+# CGPI - this field is of ratio datatype and hence the range, variance and std deviation can be obtained from it
+
+# Quality of Assigment - this data is purely qualitative in nature and thus we cannot draw any measures of spread from it
+
+
+# ----------------------------------------------------------------------------
+
+
+# TODO
+# [X] - Using previously obatined results from the analyis of the data types, find out which Measures of Spread can be applied
+
+# [ ] - Calculate the appropriate Measures of Spread for each column
+
+# [ ] - Print results
+
+
+# ----------------------------------------------------------------------------
+
+
+import pandas as pd
+
