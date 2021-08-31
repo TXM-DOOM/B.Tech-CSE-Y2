99f804da30025e2c87ce64fe1ec0ea74d6691189
Desh Iyer <desh.iyer@gmail.com>
2021-08-31 16:06:43 +0530

    Update applied-statistics/lab

--- applied-statistics/lab/expt2/experiment2.py
+++ applied-statistics/lab/expt2/experiment2.py
@@ -1,41 +1,46 @@
 # Code by Desh Iyer
 
-
 # ----------------------------------------------------------------------------
-
 
 # PROBLEM STATEMENT
 # For the data in Lab 1, find the range, standard deviation and variance for each of the variables. (Discuss measures of dispersion for Nominal and Ordinal Data)
 
-
 # ----------------------------------------------------------------------------
-
-
-# EXPLANATION
-# The data set has the following fields: Name, Marks, CGPI & Quality of Assignment
-
-# Name - this field is of nominal type. Hence no operations can be performed on it and no measures of spread can be obtained from it
-
-# Marks - this field is of ordinal type. Hence only operations of comparison can be performed. The range of this data can be found out but variance and std. deviation cannot be found.
-
-# CGPI - this field is of ratio datatype and hence the range, variance and std deviation can be obtained from it
-
-# Quality of Assigment - this data is purely qualitative in nature and thus we cannot draw any measures of spread from it
-
-
-# ----------------------------------------------------------------------------
-
 
 # TODO
 # [X] - Using previously obatined results from the analyis of the data types, find out which Measures of Spread can be applied
 
-# [ ] - Calculate the appropriate Measures of Spread for each column
+# [X] - Calculate the appropriate Measures of Spread for each column
 
-# [ ] - Print results
-
+# [X] - Print results
 
 # ----------------------------------------------------------------------------
 
-
 import pandas as pd
 
+df = pd.read_excel(r'C:\Users\Volt\code\B.Tech-CSE-Y2\applied-statistics\lab\expt2\Demo.xlsx')
+
+# Finding range of Marks column
+print('----------MARKS-----------\n')
+
+marksCol = df['Marks'].sort_values(ascending=False)
+
+marksRange = marksCol.max() - marksCol.min()
+marksStdDeviation = marksCol.std()
+marksVariance = marksCol.var()
+
+print(marksCol)
+
+print("The range of the Marks column = {}\nThe standard deviation of the Marks column = {:.2}\nThe variance of the Marks column = {:.2}".format(marksRange, marksStdDeviation, marksVariance))
+
+# Finding range, std deviation and variance from CGPA
+print('\n----------CGPA-----------\n')
+
+CGPACol = df['CGPI'].sort_values(ascending=False, ignore_index=True)
+
+CGPARange = CGPACol.max() - CGPACol.min()
+CGPAStdDeviation = CGPACol.std()
+CGPAVariance = CGPACol.var()
+
+print(CGPACol)
+print("The range of the CGPA column = {}\nThe standard deviation of the CGPA column = {:.2}\nThe variance of the CGPA column = {:.2}".format(CGPARange, CGPAStdDeviation, CGPAVariance))
