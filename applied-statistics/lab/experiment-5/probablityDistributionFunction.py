0477f9b4482f22c8c654c020da39a447ba3b8b53
Desh Iyer <desh.iyer@gmail.com>
2021-09-29 09:35:02 +0530

    Update experiment-5

--- applied-statistics/lab/experiment-5/probablityDistributionFunction.py
+++ applied-statistics/lab/experiment-5/probablityDistributionFunction.py
@@ -2,7 +2,7 @@ from scipy.integrate import quad
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-4, 4, num=100)
+x = np.linspace(-4, 4, num = 100)
 
 constant = 1.0 / np.sqrt(2 * np.pi)
 pdf_normal_distribution = constant * np.exp((-x ** 2) / 2.0)
