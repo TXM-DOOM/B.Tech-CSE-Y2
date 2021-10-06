5602c093fee7f6be805f3633a8c8489fbbc6e334
TXM-DOOM <divyamk2025@gmail.com>
2021-10-06 10:38:19 +0530

    Added Divyam s' stuff

---
+++ Divyam 's stuff/Java_Codes/asdasd.java
@@ -0,0 +1,22 @@
+public class D
+{
+    int i;
+    int j;
+    public class D(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+        }
+        public void n()
+        {
+            System.out.println("N");
+        }
+}
+public class asdasd{
+public static void main(String[] args) {
+    D d = new D();
+    d.n();
+
+    
+}
+}
